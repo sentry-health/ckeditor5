9c8c8e71d53e64617644bc272e7d3cc6ec5aa911
erik-sentry <erik@sentryhealth.ca>
2024-12-12 18:12:58 -0500

    rebuild

--- build/translations/gu.js
+++ build/translations/gu.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 !function(o){const i=o.gu=o.gu||{};i.dictionary=Object.assign(i.dictionary||{},{"%0 of %1":"","Block quote":" વિચાર ટાંકો",Bold:"ઘાટુ - બોલ્ડ્",Cancel:"","Cannot upload file:":"ફાઇલ અપલોડ ન થઇ શકી",Italic:"ત્રાંસુ - ઇટલિક્","Remove color":"","Rich Text Editor. Editing area: %0":"",Save:"","Show more items":"",Strikethrough:"",Underline:"નીચે લિટી - અન્ડરલાઇન્"}),i.getPluralForm=function(o){return 1!=o}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
-=======
-!function(n){const o=n.gu=n.gu||{};o.dictionary=Object.assign(o.dictionary||{},{"Block quote":" વિચાર ટાંકો",Bold:"ઘાટુ - બોલ્ડ્","Cannot upload file:":"ફાઇલ અપલોડ ન થઇ શકી",Italic:"ત્રાંસુ - ઇટલિક્",Strikethrough:"",Underline:"નીચે લિટી - અન્ડરલાઇન્"}),o.getPluralForm=function(n){return 1!=n}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
->>>>>>> 7d732b96267ea5f87a4f32422167c746853b299b
